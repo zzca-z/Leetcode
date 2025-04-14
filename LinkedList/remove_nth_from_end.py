f27a950b2f20601fe8f97f09628c39f16d2e0fd7
zzca-z <zhengzhouca@gmail.com>
2025-04-13 22:47:58 -0700

    LikedList_remove from nth 2 pointers

--- LinkedList/remove_nth_from_end.py
+++ LinkedList/remove_nth_from_end.py
@@ -48,7 +48,28 @@ class Solution:
         
         return head
 
+    def remove_double_pointers(self, head:Optional[ListNode], n:int) -> Optional[ListNode]:
+        temp_head = ListNode(next=head)
+        current_quick = temp_head
+        current_slow = temp_head
+        count_quick = 0
+        while count_quick < n:
+            current_quick = current_quick.next
+        while current_quick.next:
+            current_quick = current_quick.next
+            current_slow = current_slow.next
+        
+        current_slow.next = current_slow.next.next
+        
+        head = temp_head.next
+        
+        return head
+            
+        
+
+
 
 '''
 1. To decide  <, <=, length-n or length-n+1, we can use simple examples like 2 nodes to try. Because the formular must hold at 2 nodes and the format of it is fixed.
+2. Remember to update the loop termination variable
 '''
