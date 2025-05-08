fd43d8210a0c8f71753caa293bddbc236186f215
zzca-z <zhengzhouca@gmail.com>
2025-05-07 20:58:34 -0700

    Hash Anagram

---
+++ Hash/anagram.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        list_word = list(s)
+        for char in t:
+            if char in list_word:
+                list_word.remove(char)
+            else:
+                return False
+        
+        return True
