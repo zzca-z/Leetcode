6fca07be48a1b736520e96c25565f3dbfcb898bf
zzca-z <zhengzhouca@gmail.com>
2025-06-23 21:52:21 -0700

    Hash anagram, O(n) finish

--- Hash/anagram.py
+++ Hash/anagram.py
@@ -11,7 +11,26 @@ class Solution:
                 return False
         
         return True
-    
-'''
+
+    def isAnagram_hash(self, s: str, t: str) -> bool:
+        list_number = [0] * 26
+
+        for i in s:
+            list_number[ord(i) - ord('a')] += 1
+        for j in t:
+            list_number[ord(j) - ord('a')] -= 1
+        
+        if list_number == [0] * 26:
+            return True
+        else:
+            return False
+        
+        
+        
 
 '''
+1. the first implementation of list.remove() is by iterating and shifting, so the time complexity is
+O(n), so this solution is equal to brute forth with O(n^2) complexity.
+'''
+
+    
